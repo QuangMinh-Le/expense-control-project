a020af5fcb97f985ff96d21dc2ebcbf3e43a590a
Minh <quangminh.ottawa@gmail.com>
2024-10-29 16:06:21 -0400

    add paginator to expense list

--- expensecontroller/expenses/views.py
+++ expensecontroller/expenses/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Category, Expense
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -10,9 +11,13 @@ from django.contrib import messages
 def index(request):
    categories = Category.objects.all()
    expenses=Expense.objects.filter(owner=request.user)
+   paginator = Paginator(expenses, 2)
+   page_number=request.GET.get('page')
+   page_obj = Paginator.get_page(paginator, page_number)
    
    context={
       'expenses': expenses,
+      'page_obj': page_obj
    }
    return render(request, 'expenses/index.html', context)
 
@@ -41,7 +46,7 @@ def add_expense(request):
       
       if not date: 
          messages.error(request, 'Date is required')
-         return render(request, 'expenses/edit_expense.html', context)
+         return render(request, 'expenses/add_expense.html', context)
       
       Expense.objects.create(owner = request.user, amount=amount, description=description, category=category,date=date)
       messages.success(request, 'Expense saved successfully!')
