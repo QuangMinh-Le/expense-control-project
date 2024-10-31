2b29324e2773e3b896d0f18744460efb1cb3e0ea
Minh <quangminh.ottawa@gmail.com>
2024-10-31 14:50:50 -0400

    fix output from search expenses

--- expensecontroller/expensecontroller/static/js/searchExpenses.js
+++ expensecontroller/expensecontroller/static/js/searchExpenses.js
@@ -1,13 +1,18 @@
 const searchField = document.querySelector("#searchField");
 const tableOutput = document.querySelector(".table-output");
+const appTable = document.querySelector(".app-table");
+const paginationContainer = document.querySelector(".pagination-container");
+const noResult = document.querySelector(".no-result");
+const tBody = document.querySelector('.table-body');
 tableOutput.style.display = "none";
+noResult.style.display = "none";
 
 searchField.addEventListener('keyup', (e)=> {
    const searchValue = e.target.value;
 
    if (searchValue.trim().length > 0) {
-      console.log(searchValue);
-
+      paginationContainer.style.display = "none";
+      tBody.innerHTML="";
       fetch('/search-expenses', {
          body: JSON.stringify({ searchText: searchValue }),
          method: "POST",
@@ -15,13 +20,29 @@ searchField.addEventListener('keyup', (e)=> {
          .then((res) => res.json())
          .then((data) => {
             console.log("data", data);
+            appTable.style.display = "none";
             tableOutput.style.display = "block";
             if (data.length===0) {
-               
-
-
+               noResult.style.display = "block";
+               tableOutput.style.display = "none";
+            } else {
+               noResult.style.display = "none";
+               tableOutput.style.display = "block";
+               data.forEach(element => {
+                  tBody.innerHTML+= `
+                     <tr>
+                        <td>${element.amount}</td>
+                        <td>${element.category}</td>
+                        <td>${element.description}</td>
+                        <td>${element.date}</td>
+                     </tr>
+                  `
+               });
             }
-
          })
+   } else {
+      tableOutput.style.display = "none";
+      appTable.style.display = "block";
+      paginationContainer.style.display = "block";
    }
 })
